66e24ec06e897db3f96e93142a513628f28b2137
mariferxv <carol_stefany00@hotmail.com>
2024-08-12 21:32:05 -0500

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config'
+import path from "node:path"
+
+const folderName = path.basename(process.cwd())
+const mode = process.env.NODE_ENV
+const base = mode === "production" ? `/${folderName}/` : "/"
+
+// https://astro.build/config
+export default defineConfig({
+  base,
+  mode,
+  envDir: "../",
+  publicDir: "./public",
+  build: {
+    outDir: "../dist",
+    assetsDir: "./"
+  },
+  integrations: [tailwind(), react()],
+  server: {
+    host: true,
+    port: 80
+  }
+})
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,36 @@
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import './swiper.css'
+
+const Gallery = ({ images }) => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      cssMode
+      slidesPerView={1}
+      spaceBetween={30}
+      autoplay
+      navigation
+      pagination={{ clickable: true }}
+      className='w-full max-w-[80vw] h-auto md:max-h-[80svh] md:w-auto aspect-[3/4] rounded-lg'
+    >
+      {
+        images.map((image, index) =>
+          <SwiperSlide key={index}>
+            <img
+              className='size-full object-cover object-bottom rounded-lg'
+              src={image}
+              alt={`Photo ${index + 1}`}
+            />
+          </SwiperSlide>
+        )
+      }
+    </Swiper>
+  )
+}
+
+export default Gallery
